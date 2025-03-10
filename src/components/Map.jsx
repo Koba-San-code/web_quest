1f8f6fbd3459386c805d299871e3b271c6f1a0aa
Koba San <osivjos@knu.ua>
2025-03-10 16:57:07 +0200

    обновил карту

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,30 @@
-import React, { useEffect } from 'react';
-import { initMap } from './map';
+import React, { useEffect, useState } from 'react';
+import { initMap, addMapMarker } from './map';
 
 const Map = () => {
+  const [map, setMap] = useState(null);
+  
   useEffect(() => {
-    initMap();
+    const mapInstance = initMap();
+    setMap(mapInstance);
+    
+    // Обработка изменения размера окна
+    const handleResize = () => {
+      if (mapInstance) mapInstance.invalidateSize();
+    };
+    
+    window.addEventListener('resize', handleResize);
+    
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const handleMapClick = (e) => {
+    // Пример обработчика для будущего добавления меток пользователями
+    console.log('Можно добавить обработчик клика по карте здесь');
+  };
 
   return <div id="map-container" className="map-container"></div>;
 };
